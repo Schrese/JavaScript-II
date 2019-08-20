03454766a4c7ca7d2054171ffa31dde236bdcb28
Schrese <53787633+Schrese@users.noreply.github.com>
2019-08-20 18:53:30 -0400

    still working on things but need to turn in

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -56,17 +56,42 @@ function last(arr, cb) {
   })
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  // sumNums adds two numbers (x, y) and passes the result to the callback.'
+  return cb(x, y);
 }
+const add = (x, y) => {
+  return x + y;
+}
+console.log(sumNums(2, 4, add));
+
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb (x, y);
 }
+const multiply = (x, y) => {
+  return x * y;
+}
+console.log(multiplyNums(2, 3, multiply));
+
+
 
 function contains(item, list, cb) {
+
+
+
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
+
+
+
+
+
+
+
+
 
 /* STRETCH PROBLEM */
 
